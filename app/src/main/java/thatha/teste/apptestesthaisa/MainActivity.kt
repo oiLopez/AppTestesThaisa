34c597a28318c6a22126dda342eaf418d31bc13f
oiLopez <thaisavlnascimento@gmail.com>
2026-08-29 12:51:18 -0300

    Adicação de checklist de tarefas realizadas e divisão de telas de funcionamento

--- app/src/main/java/thatha/teste/apptestesthaisa/MainActivity.kt
+++ app/src/main/java/thatha/teste/apptestesthaisa/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import thatha.teste.apptestesthaisa.screens.TelaTarefas
 import thatha.teste.apptestesthaisa.ui.theme.AppTestesThaisaTheme
@@ -21,6 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppTestesThaisaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box( modifier = Modifier.padding(paddingValues = innerPadding))
                     TelaTarefas()
 
                 }
--- app/src/main/java/thatha/teste/apptestesthaisa/screens/TelaTarefas.kt
+++ app/src/main/java/thatha/teste/apptestesthaisa/screens/TelaTarefas.kt
@@ -1,7 +1,77 @@
 package thatha.teste.apptestesthaisa.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import thatha.teste.apptestesthaisa.components.ItemTarefa
+import thatha.teste.apptestesthaisa.models.Tarefa
 
 @Composable
 fun TelaTarefas() {
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+
+        var tarefas by remember {
+            mutableStateOf(
+                listOf(
+                    Tarefa(
+                        id = 1,
+                        descricao = "Estudar Kotlin"
+                    ),
+                    Tarefa(
+                        id = 2,
+                        descricao = "Praticar Composable"
+                    ),
+                    Tarefa(
+                        id = 3,
+                        descricao = "Estudar para P1"
+                    )
+                )
+            )
+        }
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            items(
+                tarefas,
+                key = { it.id }
+            ) { tarefa ->
+
+                ItemTarefa(
+                    tarefa = tarefa,
+                    onConcluidaChange = { concluida ->
+
+                        tarefas = tarefas.map {
+                            if (it.id == tarefa.id) {
+                                it.copy(concluida = concluida)
+                            } else {
+                                it
+                            }
+                        }
+
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PreviewTelaTarefas() {
+    TelaTarefas()
 }
